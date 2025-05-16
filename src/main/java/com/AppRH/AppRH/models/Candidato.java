0b605e8798973293f74f014c0f3b463bad75b686
ryan-de-melo <jos.ryan.melo@gmail.com>
2025-05-16 13:32:25 -0300

    Adicionando models

---
+++ src/main/java/com/AppRH/AppRH/models/Candidato.java
@@ -0,0 +1,69 @@
+package com.AppRH.AppRH.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+public class Candidato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotEmpty
+    private String nome;
+
+    @NotEmpty
+    @Column(unique = true)
+    private String cpf;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String telefone;
+
+    @NotEmpty
+    private String curriculo;
+
+    @ManyToOne
+    private Vaga vaga;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCurriculo() {
+        return curriculo;
+    }
+
+    public void setCurriculo(String curriculo) {
+        this.curriculo = curriculo;
+    }
+}
